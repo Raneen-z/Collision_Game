9d6f7ad03f2bd1773226bf3a6a118fac1101faac
Raneen-z <raneen.alzafarani@tuwaiq.edu.sa>
2021-04-25 17:35:15 +0300

    upload files

---
+++ SimpleCollisionGame/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace SimpleCollisionGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(543, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 61);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Me";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(335, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 77);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(330, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 181);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 522);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 522);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(350, 479);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "^";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(350, 586);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 34);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "\\/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(326, 526);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Avoid Black";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Green;
+            this.label5.Location = new System.Drawing.Point(55, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 61);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "End Point";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 666);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "<-";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SimpleCollisionGame/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCollisionGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!checkIntract(label1, label2)&& !checkIntract(label1, label3))
+            {
+                Point newPoint = new Point(label1.Location.X, label1.Location.Y - 15);
+                label1.Location = newPoint;
+
+            }
+            else
+            {
+                label4.Text = "Game Over";
+            }
+            if (checkIntract(label1, label5))
+            {
+                label4.Text = "You Won";
+                disableButton();
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!checkIntract(label1, label2) && !checkIntract(label1, label3))
+            {
+                Point newPoint = new Point(label1.Location.X+15, label1.Location.Y );
+                label1.Location = newPoint;
+
+            }
+            else
+            {
+                label4.Text = "Game Over";
+            }
+            if (checkIntract(label1, label5))
+            {
+                label4.Text = "You Won";
+                disableButton();
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!checkIntract(label1, label2) && !checkIntract(label1, label3))
+            {
+                Point newPoint = new Point(label1.Location.X, label1.Location.Y + 15);
+                label1.Location = newPoint;
+
+            }
+            else
+            {
+                label4.Text = "Game Over";
+            }
+            if (checkIntract(label1, label5))
+            {
+                label4.Text = "You Won";
+                disableButton();
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!checkIntract(label1, label2) && !checkIntract(label1, label3))
+            {
+                Point newPoint = new Point(label1.Location.X -15, label1.Location.Y);
+                label1.Location = newPoint;
+
+            }
+            else
+            {
+                label4.Text = "Game Over";
+            }
+            if (checkIntract(label1, label5))
+            {
+                label4.Text = "You Won";
+                disableButton();
+            }
+            
+        }
+
+        public bool checkIntract(Label label1, Label label2)
+        {
+            var rect1 = new System.Drawing.Rectangle(label1.Location.X, label1.Location.Y, label1.Width, label1.Height);
+            var rect2 = new System.Drawing.Rectangle(label2.Location.X, label2.Location.Y, label2.Width, label2.Height);
+            return rect1.IntersectsWith(rect2);
+          
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void disableButton()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+    }
+}
